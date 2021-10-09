95ad2b1bf2fe6d724170ca9dd2b8e7f46e4ed377
root <root@ip-172-31-13-201.ap-south-1.compute.internal>
2021-10-09 05:36:37 +0000

    second commit

--- first.java
+++ first.java
@@ -2,6 +2,6 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("This is my first java program executed in Jenkins");
     }
 }
